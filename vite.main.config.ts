ad10a4d7f8a97d357d70d27ba5a7f3cf44f3374f
Qi Xiao <qi@imbue.com>
2026-01-27 13:20:51 +0000

    Fix building

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,17 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        outDir: ".vite/build",
+        lib: {
+            entry: "src/main.ts",
+            formats: ["cjs"],
+            fileName: () => "main.cjs"
+        },
+        rollupOptions: {
+            external: ["electron"]
+        }
+    }
+
+});
